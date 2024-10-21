c3d3d16294c01c8474299a6e7c5a1886e85f7811
mrshiva03 <shivamisha09@gmail.com>
2024-10-21 12:28:22 +0530

    new

--- Calculater.py
+++ Calculater.py
@@ -4,9 +4,22 @@ import streamlit as st
 
 st.title("calculater")
 st.markdown("Welcome to my streamlit app")
-c1, c2 = st.column(2)
+c1, c2 = st.columns(2)
 fnum = c1.number_input("Enter first number", value=1)
-num = c1.number_input("Enter second number", value=0)
+snum = c2.number_input("Enter second number", value=0)
 
 options = ["Add", "Subtract", "Multiply", "Divide"]
 choice = st.radio("select operation", options)
+
+button = st.button("calculate")
+if button:
+    if choice == "Add":
+        result = fnum + snum
+    if choice == "Subtract":
+        result = fnum - snum
+    if choice == "Multiply":
+        result = fnum * snum
+    if choice == "Divide":
+        result = fnum // snum
+
+st.warning(f"the result is: {result}")
